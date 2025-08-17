8385646eb83a6434df6b38ad40211cdc465b24ca
Sabrina Brown <shardena26@gmail.com>
2025-08-17 20:42:16 +0100

    Updated API and added functions

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+    let apiKey = "7f85db0t4bd30o2cf66ff776ea0a2420";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+  
+    searchCity(searchInput.value);
 }
 
 
